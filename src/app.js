b913c36ac71640c84b4b5ce55128f5e7d6b291c7
George Padmore Yeboah <padmorey@gmail.com>
2022-02-09 18:25:08 +0000

    /balances/deposit/:userId done

--- src/app.js
+++ src/app.js
@@ -83,6 +83,66 @@ app.get('/contracts',getProfile ,async (req, res) =>{
 
 
 /**
+ * Deposits money into balance of a client, a client can't deposit more than 25% his total of jobs to pay. (at the deposit moment)
+ * @returns response of payment (success or failure)
+ */
+ app.post('/balances/deposit/:userId',getProfile ,async (req, res) =>{
+    const {Job} = req.app.get('models')
+    const {Contract, Profile} = req.app.get('models')
+    const {id, balance, type} = req.profile //retreive profile ID
+    client_id = req.params.userId
+    const deposit_amount = req.body.deposit_amount
+    let total_amount_unpaid = 0;
+
+    var resp_code = "";
+    var resp_desc = "";
+
+    const job = await Job.findAll({
+        where: {paid: { [Op.is]: null },},
+        attributes: [
+            [sequelize.fn('sum', sequelize.col('price')), 'total_amount_unpaid'],
+        ],
+        include: [{
+          model: Contract,
+          where: { status: 'in_progress', ClientId: client_id},
+        }],
+        raw: true,
+      }).then(function(job_result) {
+          total_amount_unpaid = job_result[0].total_amount_unpaid
+        return job_result
+      })
+
+      if (deposit_amount <= (0.25 * total_amount_unpaid) ) {
+       
+        const t = await sequelize.transaction();
+    
+        try {
+
+            Profile.update({ balance: sequelize.literal(`balance + ${deposit_amount}`) }, { where: { id: client_id }}, { transaction: t });
+            
+            await t.commit();
+
+            resp_code = "000"
+            resp_desc = `Deposit of ${deposit_amount} has been made successfully.`
+            
+        } catch (error) {
+            // If the execution reaches this line, an error was thrown.
+            // We rollback the transaction.
+            await t.rollback();
+
+            resp_code = "999"
+            resp_desc = `Deposit of ${deposit_amount} failed. Please try again.`
+        }
+
+      }
+
+    
+    res.json({ resp_code: resp_code, resp_desc: resp_desc});
+})
+
+
+
+/**
  * Pay for a job
  * @returns response of payment (success or failure)
  */
@@ -150,7 +210,5 @@ app.get('/contracts',getProfile ,async (req, res) =>{
     
     res.json({ resp_code: resp_code, resp_desc: resp_desc});
 })
-
-
 
 module.exports = app;
